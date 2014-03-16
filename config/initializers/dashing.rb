048a16de77c9d5e0bd04ca500abe46fd135606b1
digidigo <digidigo@gmail.com>
2014-03-15 21:31:58 -0600

    prod redis

--- config/initializers/dashing.rb
+++ config/initializers/dashing.rb
@@ -5,8 +5,8 @@ Dashing.configure do |config|
 
   # Redis credentials.
   # See https://devcenter.heroku.com/articles/redistogo to configure redis for heroku.
-  # config.redis_host     = '127.0.0.1'
-  # config.redis_port     = '6379'
+  config.redis_host     = 'rediscache.embomd.0001.use1.cache.amazonaws.com'
+  config.redis_port     = '6379'
   # config.redis_password = nil
 
   # Redis namespace when pushing new data.
