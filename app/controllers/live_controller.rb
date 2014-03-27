1514829e2d1e61bd6d2d0d6961a361077ae139b1
digidigo <digidigo@gmail.com>
2014-03-27 10:21:45 -0700

    try 5 times

--- app/controllers/live_controller.rb
+++ app/controllers/live_controller.rb
@@ -4,13 +4,18 @@ class LiveController < ApplicationController
   def feed
     response.headers['Content-Type']      = 'text/event-stream'
     response.headers['X-Accel-Buffering'] = 'no'
-
+    i = 0
     while true do 
       response.stream.write "id: 0\n"
       response.stream.write "event: update\n"
       data = {time: Time.now.to_s}.to_json
       response.stream.write "data: #{data}\n\n"
       sleep 2
+      
+      i++
+      break if i > 5
     end
+    
+    response.stream.close
   end
 end
